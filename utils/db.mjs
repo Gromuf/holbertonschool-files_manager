834dd4fd75664d6a1b815ef55c6d3d51e31b2656
Gromuf <luigi.beaumois@gmail.com>
2026-01-05 11:33:51 +0100

    style: standardize quotes in db.mjs to single quotes

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,13 +1,13 @@
 // db.mjs
 
-import mongodb from "mongodb";
+import mongodb from 'mongodb';
 
 const { MongoClient } = mongodb;
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || "localhost";
+    const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || "files_manager";
+    const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
     this.client = new MongoClient(url, {
       useNewUrlParser: true,
@@ -27,12 +27,12 @@ class DBClient {
 
   async nbUsers() {
     if (!this.db) return 0;
-    return this.db.collection("users").countDocuments();
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
     if (!this.db) return 0;
-    return this.db.collection("files").countDocuments();
+    return this.db.collection('files').countDocuments();
   }
 }
 const dbClient = new DBClient();
