e3ba4c5d0599b06ec6fcd694eaf9ea5b47889b6c
Gromuf <luigi.beaumois@gmail.com>
2026-01-06 10:35:09 +0100

    style: reorder variable declarations for better organization in server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // server.js
 const express = require('express');
-const app = express();
-
-const port = process.env.PORT || 5000;
 const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/', routes);
