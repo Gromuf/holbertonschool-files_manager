29c8c2274e91c4357052e2ff1d91515f9bac34e3
Gromuf <luigi.beaumois@gmail.com>
2026-01-06 10:30:09 +0100

    style: reorder import statements for better organization in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 // index.js
 
 const express = require('express');
-const router = express.Router();
 const AppController = require('../controllers/AppController');
+
+const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 module.exports = router;
