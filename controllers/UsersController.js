e79429d27a658b8f74ccdba8236601979cf09c4d
Gromuf <luigi.beaumois@gmail.com>
2026-01-06 11:04:38 +0100

    feat: implement UsersController with postNew method for user registration

---
+++ controllers/UsersController.js
@@ -0,0 +1,44 @@
+// UsersController.js
+import sha1 from 'sha1';
+const redisClient = require('../utils/redis').default;
+const dbClient = require('../utils/db').default;
+
+const waitForDB = async () => {
+  while (!dbClient.db) {
+    await new Promise((resolve) => setTimeout(resolve, 50));
+  }
+};
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    await waitForDB();
+
+    const user = await dbClient.db.collection('users').findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+
+    const result = await dbClient.db.collection('users').insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      id: result.insertedId,
+      email,
+    });
+  }
+}
+
+module.exports = UsersController;
