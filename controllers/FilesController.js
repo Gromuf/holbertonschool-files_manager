b19df919d94036bb3ea76973b86916855e4901e2
Gromuf <luigi.beaumois@gmail.com>
2026-01-09 10:36:55 +0100

    fix: correct userId type in getShow method for accurate file retrieval

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -88,14 +88,14 @@ class FilesController {
       }
       const file = await dbClient.db.collection('files').findOne({
         _id: new ObjectId(id),
-        userId,
+        userId: new ObjectId(userId),
       });
       if (!file) {
         return res.status(404).json({ error: 'Not found' });
       }
       return res.status(200).json({
         id: file._id.toString(),
-        userId: file.userId,
+        userId: file.userId.toString(),
         name: file.name,
         type: file.type,
         isPublic: file.isPublic,
